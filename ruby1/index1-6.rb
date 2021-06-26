9f63cd0d2fed3dfd5bd6e6b511b4b4b827afaad3
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-06-26 20:52:51 +0900

    配列要素の挿入

---
+++ ruby1/index1-6.rb
@@ -0,0 +1,52 @@
+# alpha = %w(a b c d e f g)
+# p alpha
+
+# alpha[2, 3] = %w(C D E)
+# p alpha
+
+# puts
+
+# alpha = %w(a b c d e f g)
+# p alpha
+
+# alpha[2..4] = %w(C D E)
+# p alpha
+
+# alpha = %w(a b c d e f g)
+# p alpha
+
+# alpha[7, 0] = %w(X Y Z)
+# p alpha
+
+# alpha = %w(a b c d e f g)
+# p alpha.values_at(1, 3, 5)
+
+# alpha = %w(a b c d e f g)
+# p alpha.map {|a| a.upcase}
+
+# a = "ruby ruby"
+# p a.upcase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
