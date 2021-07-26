d4b65701ac224566e7575d87ce9ff9325ec28e1a
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-26 21:09:57 +0900

    約数を求める

--- ruby1/index1-6.rb
+++ ruby1/index1-6.rb
@@ -448,50 +448,59 @@
 # puts two_sum([2, 6, 11, 15], 17)
 
 
-num1 = [1, 2, 3]
+# num1 = [1, 2, 3]
 
-num1.each_with_index do |n, a|
-  num1.each_with_index do |u, b|
-    puts
-    puts n
-    puts a
-    puts u
-    puts b
-    puts
-  end
-end
-
-
+# num1.each_with_index do |n, a|
+#   num1.each_with_index do |u, b|
+#     puts
+#     puts n
+#     puts a
+#     puts u
+#     puts b
+#     puts
+#   end
+# end
 
 
+# def sum_of_pythagoras(sum)
+#   a = 1; b, c = a+1, a+2
+#   limit = sum
+#   loop do
+#     if (a + b + c) == sum and pythagoras?(a, b, c)
+#       return a, b, c
+#     end
+#     c += 1
+#     if c > limit
+#       b += 1; c = b + 1
+#     end
+#     if b > limit/2
+#       a += 1; b = a + 1; c = b + 1
+#     end
+#     return nil if a > limit/3
+#   end
+# end
+# def pythagoras?(a, b, c)
+#   return true if (a ** 2 + b ** 2) == c ** 2
+#   false
+# end
 
+# a, b, c = sum_of_pythagoras(1000)
+# puts "#{a} * #{b} * #{c} = #{a*b*c}"
 
+# def yakusu(num)
+#   i = 1
+#   result = []
+#   while i <= num do
+#     ans = num % i
+#     if ans == 0
+#        result << i
+#     end
+#     i += 1
+#   end
+#   return result
+# end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# p yakusu(15)
 
 
 
