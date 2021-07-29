3d2b0d9c110cef8d59d9f4af57ce08f7cc4eb2e8
kanaokaseiya <ttf.1998.seiya@gmail.com>
2021-07-29 20:45:17 +0900

    累乗のループ文

--- ruby1/index1-6.rb
+++ ruby1/index1-6.rb
@@ -1034,3 +1034,15 @@
 
 # puts "終了します。"
 
+#   def total_count_for(amount)
+#     if amount.zero?
+#       0
+#     elsif amount.odd?
+#       3 * amount / 2
+#     elsif amount.even?
+#       3 * (amount - 1) / 2 + 1
+#     end
+#   end
+
+# puts  total_count_for(12)
+
---
+++ ruby1/index1-7.rb
@@ -0,0 +1,14 @@
+# def loops(a, n)
+#   b = 1
+#   n.times do
+#     b *= a
+#   end
+#   return b
+# end
+# t = Time.now
+# puts loops(3, 4)
+# puts Time.now - t
+
+
+
+
